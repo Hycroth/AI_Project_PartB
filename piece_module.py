02281e41b5aa3d3133f6f145cc8c8e8d427b7c6d
Ckyever <ckyeverg@gmail.com>
2018-04-22 19:59:59 +1000

    Initial skeleton code

---
+++ piece_module.py
@@ -0,0 +1,15 @@
+# Piece module for COMP30024: Artificial Intelligence, 2018
+# Authors: Ckyever Gaviola, Samuel Fatone
+
+class Player:
+    def __init__(self, colour):
+        self.colour = colour
+        
+    ## Returns next move
+    def action(self, turns):
+        
+        return next_action
+    
+    ## Update game board with opponents move
+    def update(self, action):
+        
