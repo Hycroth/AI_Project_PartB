660410dbd5a97da54bc4a8fd9e4f6ac863787a1d
Ckyever <ckyeverg@gmail.com>
2018-04-29 19:17:16 +1000

    Piece can check if it is eliminated

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 from player_module import Player, Board, Piece
 
 white = Player('white')
-white.board.place_piece(white.colour, (1,0))
+white.board.place_piece('O', (2,0))
+white.board.place_piece('@', (1,0))
+white.board.print_board()
+white.board.black_pieces[0].check_eliminated()
 white.board.print_board()
