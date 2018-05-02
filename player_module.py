8be0d9d2a1f6c0026ebf5e7d26e56cf4d2676c5b
Ckyever <ckyeverg@gmail.com>
2018-05-03 00:14:50 +1000

    Added default board size and some comments

--- player_module.py
+++ player_module.py
@@ -3,17 +3,23 @@
 
 from watchyourback import Board, Piece
 
+DEFAULT_BOARD_SIZE = 8
+
 class Player:
     def __init__(self, colour):
         if colour == 'white':
             self.colour = 'O'
         if colour == 'black':
             self.colour = '@'
-        self.board = Board()
+        self.board = Board(DEFAULT_BOARD_SIZE)
         
     ## Returns next move
     def action(self, turns):
+        # Check if board shrinks
         
+        # Placing phase
+        
+        # Moving phase
         return next_action
     
     ## Update game board with opponents move
